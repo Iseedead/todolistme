e2315464d6e38e382e04033a2608df04fb7295b8
Pasha S <sivkovych.p@gmail.com>
2017-10-14 21:42:28 +0300

    Refactor.

--- src/main/java/com/haman/lessons/core/pageObject/panel/listPanel/ListManagerPanel.java
+++ src/main/java/com/haman/lessons/core/pageObject/panel/listPanel/ListManagerPanel.java
@@ -8,13 +8,6 @@ import static com.codeborne.selenide.Condition.text;
 
 public class ListManagerPanel implements ListManagerPanelable {
 
-    /*
-    * I really messed up with xPath selectors here.
-    * I truly believe they can be much better and stylish then mine.
-    * Also I'm not sure about this architecture.
-    * Maybe it will be better if I separate this class into similar two -- one for Categories and one for Lists.
-     */
-
     @FindBy(xpath = "//div[@id='lists']/ul/li/*/*")
     private ElementsCollection lists;
 
@@ -32,8 +25,8 @@ public class ListManagerPanel implements ListManagerPanelable {
         getNodeByName(listName).$x("span").click();
     }
 
-    public SelenideElement containingCategory(String listName) {
-        return getNodeByName(listName).$x("../../h5/span");
+    public String containingCategory(String listName) {
+        return getNodeByName(listName).$x("../../h5/span").getText();
     }
 
 }
--- src/test/java/com/haman/lessons/TestTest.java
+++ src/test/java/com/haman/lessons/TestTest.java
@@ -4,9 +4,9 @@ import com.haman.lessons.core.pageObject.page.MainPage;
 import com.haman.lessons.core.pageObject.panel.listPanel.ListManagerPanel;
 import com.haman.lessons.core.pageObject.panel.taskPanel.CompletePanel;
 import com.haman.lessons.core.pageObject.panel.taskPanel.ToDoPanel;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.page;
 
 
@@ -19,26 +19,26 @@ public class TestTest extends BaseTest {
     @Test
     public void taskCreationTest() throws InterruptedException {
         page(MainPage.class).createNewTask(taskName);
-        page(ToDoPanel.class).getToDoByName(taskName).should(be(visible));
+        Assert.assertTrue(page(ToDoPanel.class).getToDoByName(taskName).isDisplayed());
     }
 
     @Test
     public void taskCompletionTest() throws InterruptedException {
         page(MainPage.class).createNewTask(taskName);
         page(ToDoPanel.class).completeTask(taskName);
-        page(CompletePanel.class).getToDoByName(taskName).should(be(visible));
+        Assert.assertTrue(page(CompletePanel.class).getToDoByName(taskName).isDisplayed());
     }
 
     @Test
     public void categoryCreationTest() throws InterruptedException {
         page(MainPage.class).createNewCategory(categoryName);
-        page(ListManagerPanel.class).getNodeByName(categoryName).should(be(visible));
+        Assert.assertTrue(page(ListManagerPanel.class).getNodeByName(categoryName).isDisplayed());
     }
 
     @Test
     public void listCreationTest() throws InterruptedException {
         page(MainPage.class).createNewList(listName);
-        page(ListManagerPanel.class).getNodeByName(listName).should(be(visible));
+        Assert.assertTrue(page(ListManagerPanel.class).getNodeByName(listName).isDisplayed());
     }
 
     @Test
@@ -46,16 +46,8 @@ public class TestTest extends BaseTest {
         page(MainPage.class).createNewCategory(categoryName);
         page(MainPage.class).createNewList(listName);
         page(ListManagerPanel.class).moveListToCategory(listName, categoryName);
-        page(ListManagerPanel.class).containingCategory(listName).should(have(textCaseSensitive(categoryName)));
+        Assert.assertEquals(page(ListManagerPanel.class).containingCategory(listName), categoryName);
     }
-
-    /*
-    * Don't really know if I should divide this Test ↓ into separate once.
-    * Feel like I should, but at the same time it will be A LOT OF repeated code.
-    * for example, I already have Test for task completion. Maybe I just don't have to CHECK it.
-    * but again, BUT, this is Test after moving task, so this isn't the same shit as before
-    * all in all, i'm just another brick in the wall
-     */
 
     @Test
     public void toDoListIntegrationTest() throws InterruptedException {
@@ -70,11 +62,11 @@ public class TestTest extends BaseTest {
         mPage.createNewTask(taskName);
         tdPanel.moveToList(taskName, listName);
         lmPanel.goToList(listName);
-        tdPanel.getToDoByName(taskName).should(be(visible));
+        Assert.assertTrue(tdPanel.getToDoByName(taskName).isDisplayed());
         tdPanel.completeTask(taskName);
-        cPanel.getToDoByName(taskName).should(be(visible));
+        Assert.assertTrue(cPanel.getToDoByName(taskName).isDisplayed());
         cPanel.deleteTodoByName(taskName);
-        cPanel.getCompletes().shouldHaveSize(0);
+        Assert.assertTrue(cPanel.getCompletes().isEmpty());
     }
 
 }
